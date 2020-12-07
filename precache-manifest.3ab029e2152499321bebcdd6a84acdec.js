cbdf0d10251129262e255cb7a9d2c9d36368c3be
germanvnwizeline <german.villalpando@wizeline.com>
2020-12-06 23:50:44 -0600

    Updates

---
+++ precache-manifest.3ab029e2152499321bebcdd6a84acdec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06e3c0224a09da8800af9bb5072ea58",
+    "url": "/react-bootcamp-2020/index.html"
+  },
+  {
+    "revision": "8f74839d4bf41793b00e",
+    "url": "/react-bootcamp-2020/static/css/main.6968fbd9.chunk.css"
+  },
+  {
+    "revision": "147481cdf90ee1a8cb4e",
+    "url": "/react-bootcamp-2020/static/js/2.48361287.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/react-bootcamp-2020/static/js/2.48361287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f74839d4bf41793b00e",
+    "url": "/react-bootcamp-2020/static/js/main.5e2fa022.chunk.js"
+  },
+  {
+    "revision": "eeb19e06e59dded179e1",
+    "url": "/react-bootcamp-2020/static/js/runtime-main.2724eb93.js"
+  }
+]);
